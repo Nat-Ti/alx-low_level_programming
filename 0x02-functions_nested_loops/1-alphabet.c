45906ed50ab0c112a2963d51ca21c36fa8651aca
Nati Ti <88322661+Nat-Ti@users.noreply.github.com>
2021-08-31 15:40:46 +0300

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c = c + 1;
+}
+_putchar('\n');
+}
