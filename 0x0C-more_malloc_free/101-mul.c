d982d5c2876d1fb1c9dd77f9a2eb2dfc2b55e9f3
Nati Ti <88322661+Nat-Ti@users.noreply.github.com>
2021-09-23 14:08:39 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,84 +1,156 @@
-#include <stdlib.h>
 #include "main.h"
-/**
-  * string_nconcat - concatenates two strings.
-  * @s1: pointer to first string.
-  * @s2: pointer to second string from which we'll take first n bytes.
-  * @n: n bytes of s2.
-  *
-  * Return: pointer to newly allocated space in memory.
-  * NULL if malloc fails.
-  */
- * _strlen - get length of string.
- * @str: string.
- * Return: length.
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		++i;
-	}
-	return (i);
+/**
+ * str_len - finds string length
+ * @str: input pointer to string
+ * Return: length of string
+ */
+int str_len(char *str)
+{
+	int len;
+
+	for (len = 0; *str != '\0'; len++)
+		len++, str++;
+	return (len / 2);
 }
 /**
- * string_nconcat - concatenates two strings.
- * @s1: strging 1.
- * @s2: string 2.
- * @n: byets of s2 to include.
- * Return: pointer to a string or NULL.
+ * _calloc - allocates memory for an array using malloc
+ * @bytes: bytes of memory needed per size requested
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int bytes, unsigned int size)
 {
-	char *nstr;
-	unsigned int len1, len2, size, i, j;
-	char *str;
-	unsigned int len1, len2, i, j;
+	unsigned int i;
+	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s2[len2] != '\0')
-		len2++;
-
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	if (n >= len2)
-		n = len2;
-	size = len1 + n;
-
-	nstr = malloc((sizeof(char) * size) + 1);
-	if (nstr == NULL)
-	str = malloc(len1 + n + 1 * sizeof(char));
-	if (str == NULL)
+	if (bytes == 0 || size == 0)
 		return (NULL);
-	i = 0;
-	while (i < len1)
+	if (size >= UINT_MAX / bytes || bytes >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * bytes);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < bytes * size; i++)
+		p[i] = 0;
+	return ((void *)p);
+}
+/**
+ * add_arrays - adds 2 arrays of ints
+ * @mul_result: pointer to array with numbers from product
+ * @sum_result: pointer to array with numbers from total sum
+ * @len_r: length of both arrays
+ * Return: void
+ */
+void add_arrays(int *mul_result, int *sum_result, int len_r)
+{
+	int i = 0, len_r2 = len_r - 1, carry = 0, sum;
+
+	while (i < len_r)
 	{
-		nstr[i] = s1[i];
+		sum = carry + mul_result[len_r2] + sum_result[len_r2];
+		sum_result[len_r2] = sum % 10;
+		carry = sum / 10;
 		i++;
+		len_r2--;
 	}
-	j = 0;
-	while (i < size)
+}
+/**
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	printf("Error\n");
+	return (0);
+}
+/**
+ * multiply - multiplies 2 #'s, prints result, must be 2 #'s
+ * @num1: factor # 1 (is the smaller of 2 numbers)
+ * @len_1: length of factor 1
+ * @num2: factor # 2 (is the larger of 2 numbers)
+ * @len_2: length of factor 2
+ * @len_r: length of result arrays
+ * Return: 0 fail, 1 success
+ */
+int *multiply(char *num1, int len_1, char *num2, int len_2, int len_r)
+{
+	int i = 0, i1 = len_1 - 1;
+	int i2, product, carry, digit, *mul_result, *sum_result;
+
+	sum_result = _calloc(sizeof(int), (len_r));
+	while (i < len_1)
 	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
+		mul_result = _calloc(sizeof(int), len_r);
+		i2 = len_2 - 1, digit = (len_r - 1 - i);
+		if (!is_digit(num1[i1]))
+			return (NULL);
+		carry = 0;
+		while (i2 >= 0)
+		{
+			if (!is_digit(num2[i2]))
+				return (NULL);
+			product = (num1[i1] - '0') * (num2[i2] - '0');
+			product += carry;
+			mul_result[digit] += product % 10;
+			carry = product / 10;
+			digit--, i2--;
+		}
+		add_arrays(mul_result, sum_result, len_r);
+		free(mul_result);
+		i++, i1--;
 	}
-	nstr[i] = '\0';
-	return (nstr);
-	for (i = 0; i < len1; ++i)
-		str[i] = s1[i];
-	for (j = 0; j < n; ++j, ++i)
-		str[i] = s2[j];
-	str[i] = '\0';
-	return (str);
+	return (sum_result);
+}
+/**
+ * print_me - prints my array of the hopeful product here
+ * @sum_result: pointer to int array with numbers to add
+ * @len_r: length of result array
+ * Return: void
+ */
+void print_me(int *sum_result, int len_r)
+{
+	int i = 0;
+
+	while (sum_result[i] == 0 && i < len_r)
+		i++;
+	if (i == len_r)
+		_putchar('0');
+	while (i < len_r)
+		_putchar(sum_result[i++] + '0');
+	_putchar('\n');
+}
+/**
+ * main - multiply 2 input #'s of large lengths and print result or print Error
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, Success
+ */
+int main(int argc, char **argv)
+{
+	int len_1, len_2, len_r, temp, *sum_result;
+	char *num1, *num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	len_1 = str_len(argv[1]), len_2 = str_len(argv[2]);
+	len_r = len_1 + len_2;
+	if (len_1 < len_2)
+		num1 = argv[1], num2 = argv[2];
+	else
+	{
+		num1 = argv[2], num2 = argv[1];
+		temp = len_2, len_2 = len_1, len_1 = temp;
+	}
+	sum_result = multiply(num1, len_1, num2, len_2, len_r);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result, len_r);
+	return (0);
 }
